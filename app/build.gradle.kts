d3dde137a2ec1a48d720df10fc13d24e3a2c9872
Jacob Salvi <jacob.salvi@usi.ch>
2023-11-17 19:02:34 +0100

    Start adding camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mwcproject"
+    namespace = "com.example.stepappv4"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.mwcproject"
-        minSdk = 33
+        applicationId = "com.example.stepappv4"
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -19,24 +19,33 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-fragment:2.7.0-alpha01")
+    implementation("androidx.navigation:navigation-ui:2.7.0-alpha01")
+    implementation ("com.github.AnyChart:AnyChart-Android:1.1.5")
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+    implementation ("androidx.camera:camera-view:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
 }
---
+++ app/src/main/java/com/example/stepappv4/ui/Camera/CameraFragment.java
@@ -0,0 +1,88 @@
+package com.example.stepappv4.ui.Camera;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.stepappv4.R;
+import com.example.stepappv4.databinding.CameraFragmentBinding;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+public class CameraFragment extends Fragment {
+
+    private CameraFragmentBinding binding;
+    private PreviewView previewView;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = CameraFragmentBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        previewView = root.findViewById(R.id.previewView);
+        int previewView = R.id.previewView;
+        System.out.println("Root is " +root.getDisplay());
+//        if (hasCameraPermission()) {
+//            enableCamera();
+//        } else {
+//            requestPermission();
+//        }
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        this.startCamera();
+
+    }
+
+    private void startCamera(){
+        System.out.println("startcamera");
+        ImageCapture imageCapture =
+                new ImageCapture.Builder()
+                        .setTargetRotation(binding.getRoot().getDisplay().getRotation())
+                        .build();
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture =  ProcessCameraProvider.getInstance(this.getContext());
+        cameraProviderFuture.addListener(()->{
+            ProcessCameraProvider cameraProvider = null;
+            try {
+                cameraProvider = cameraProviderFuture.get();
+            } catch (ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            bindPreview(cameraProvider);
+        }, ContextCompat.getMainExecutor(this.getContext()));
+
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider pv){
+        System.out.println("bindpreview");
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cs = new CameraSelector.Builder().requireLensFacing(
+                CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        Camera camera = pv.bindToLifecycle(this, cs, preview);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
