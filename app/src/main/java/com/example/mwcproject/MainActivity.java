537e90ea0228cc6dfdcc45b035c33ee90460ee7f
MS <JAS1997@outlook.it>
2023-11-19 11:25:30 +0100

    Localisation service maps (to improve) ans ask for permission to user (code to clean

--- app/src/main/java/com/example/mwcproject/MainActivity.java
+++ app/src/main/java/com/example/mwcproject/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.mwcproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.mwcproject.databinding.ActivityMainBinding;
 import com.example.mwcproject.fragments.maps.DownButtons.DownButtonFragment;
 import com.example.mwcproject.fragments.maps.maps.MapsFragment;
+import com.example.mwcproject.services.LocationService;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 101;
+
 
     ActivityMainBinding binding;
     @Override
@@ -16,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        requestLocationPermissions();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container_map, MapsFragment.class, null)
@@ -29,4 +39,33 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
     }
+
+
+
+    private void requestLocationPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            startLocationService();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationService();
+            } else {
+                //Todo: Handle the case where the user denies the permission
+            }
+        }
+    }
+
+    private void startLocationService() {
+        Intent serviceIntent = new Intent(this, LocationService.class);
+        startService(serviceIntent);
+    }
+
 }
--- app/src/main/java/com/example/mwcproject/services/LocationService.java
+++ app/src/main/java/com/example/mwcproject/services/LocationService.java
@@ -1,47 +1,57 @@
 package com.example.mwcproject.services;
 
-
 import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.Bundle;
+import android.os.Binder;
+import android.os.IBinder;
 import android.os.Looper;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
-import com.example.mwcproject.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
-import androidx.annotation.NonNull;
-
-public class LocationService extends AppCompatActivity {
-
+public class LocationService extends Service {
+    private final IBinder binder = new LocalBinder();
     private FusedLocationProviderClient fusedLocationClient;
+    private Location currentLocation;
+    private LocationUpdateListener locationUpdateListener;
     private LocationCallback locationCallback;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        System.out.println("HERE 1");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
-        } else {
-            setupLocationListener();
+    public class LocalBinder extends Binder {
+        public LocationService getService() {
+            return LocationService.this;
         }
     }
 
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        setupLocationListener();
+    }
+
+    public interface LocationUpdateListener {
+        void onLocationChanged(Location location);
+    }
+
+    public void setLocationUpdateListener(LocationUpdateListener listener) {
+        this.locationUpdateListener = listener;
+    }
+
     private void setupLocationListener() {
-        System.out.println("HERE 2");
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setInterval(10000); // Update interval
         locationRequest.setFastestInterval(5000); // Fastest update interval
@@ -50,23 +60,24 @@ public class LocationService extends AppCompatActivity {
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
-                Location mostAccurateLocation = null;
-                float bestAccuracy = Float.MAX_VALUE;
                 for (Location location : locationResult.getLocations()) {
                     if (location.hasAccuracy()) {
                         float accuracy = location.getAccuracy();
-                        if (accuracy < bestAccuracy) {
-                            mostAccurateLocation = location;
-                            bestAccuracy = accuracy;
+                        if (currentLocation == null || accuracy < currentLocation.getAccuracy()) {
+                            currentLocation = location;
+                            if (locationUpdateListener != null) {
+                                locationUpdateListener.onLocationChanged(location);
+                            }
                         }
                     }
-
                 }
-                System.out.println(mostAccurateLocation);
-                LocationManager.getInstance().setLocation(mostAccurateLocation);
             }
         };
 
+        startLocationUpdates(locationRequest);
+    }
+
+    private void startLocationUpdates(LocationRequest locationRequest) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -80,27 +91,15 @@ public class LocationService extends AppCompatActivity {
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
     }
 
-    private void updateLocation(Location location) {
-
-    }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 101) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setupLocationListener();
-            } else {
-                // Handle permission denial
-            }
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         if (fusedLocationClient != null && locationCallback != null) {
             fusedLocationClient.removeLocationUpdates(locationCallback);
         }
     }
+
+
+
 }
+
