14e67e2c69d4eff867c537c008ef6f15bf751db3
MS <JAS1997@outlook.it>
2023-11-21 15:34:58 +0100

    connect button camera to view partial

--- app/src/main/java/com/example/mwcproject/MainActivity.java
+++ app/src/main/java/com/example/mwcproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mwcproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -24,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         requestLocationPermissions();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
--- app/src/main/java/com/example/mwcproject/fragments/DownButtonFragment.java
+++ app/src/main/java/com/example/mwcproject/fragments/DownButtonFragment.java
@@ -1,15 +1,50 @@
 package com.example.mwcproject.fragments;
 
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.LifecycleObserver;
 
 import com.example.mwcproject.R;
+import com.example.mwcproject.databinding.CameraFragmentBinding;
+import com.example.mwcproject.databinding.DownButtonFragmentBinding;
 
 public class DownButtonFragment extends Fragment {
+
+    private FragmentManager fm;
+    DownButtonFragmentBinding binding;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        super.onCreateView(inflater, container, savedInstanceState);
+        binding = DownButtonFragmentBinding.inflate(inflater, container, false);
+        System.out.println("ALPACA before");
+        binding.cameraBtn.setOnClickListener(view -> {
+                    System.out.println("ALPACA");
+                    getParentFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container_map, MapsFragment.class, null)
+                            .setReorderingAllowed(true)
+                            .addToBackStack(null) // if you want to add it to the back stack
+                            .commit();
+                }
+        );
+        return binding.getRoot().findViewById(R.id.down_bts);
+    }
+
     public DownButtonFragment() {
         super(R.layout.down_button_fragment);
 
-
     }
-
 
 }
